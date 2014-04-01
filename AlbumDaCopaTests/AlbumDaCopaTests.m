8ab57f12c5c405e27c7a7693e7a72da271ac9dbd
rafagonc77@yahoo.com.br <rafagonc77@yahoo.com.br>
2014-03-31 23:02:42 -0300

    Initial Commit

---
+++ AlbumDaCopaTests/AlbumDaCopaTests.m
@@ -0,0 +1,34 @@
+//
+//  AlbumDaCopaTests.m
+//  AlbumDaCopaTests
+//
+//  Created by Rafael Gonzalves on 3/31/14.
+//  Copyright (c) 2014 Rafael Gonzalves. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlbumDaCopaTests : XCTestCase
+
+@end
+
+@implementation AlbumDaCopaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
